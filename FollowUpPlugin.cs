98e93e923494e57352cc87ba6dd8bfdef4b81b74
yamgarcia <yamgarcia.ca@gmail.com>
2022-12-08 15:28:24 -0800

    First commit

---
+++ FollowUpPlugin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace BasicPlugin
+{
+    public class FollowUpPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            tracingService.Trace("Plugin Start");
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+                if (entity.LogicalName == "account")
+                {
+                    //add business logic here
+
+
+                    // Obtain the organization service reference which you will need for  
+                    // web service calls.  
+                    IOrganizationServiceFactory serviceFactory =
+                        (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    try
+                    {
+                        // Create a task activity to follow up with the account customer in 7 days. 
+                        Entity followup = new Entity("task");
+
+                        followup["subject"] = "Send e-mail to the new customer.";
+                        followup["description"] =
+                            "Follow up with the customer. Check if there are any new issues that need resolution.";
+                        followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                        followup["scheduledend"] = DateTime.Now.AddDays(7);
+                        followup["category"] = context.PrimaryEntityName;
+
+                        // Refer to the account in the task activity.
+                        if (context.OutputParameters.Contains("id"))
+                        {
+                            Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                            string regardingobjectidType = "account";
+
+                            followup["regardingobjectid"] =
+                            new EntityReference(regardingobjectidType, regardingobjectid);
+                        }
+
+                        // Create the task in Microsoft Dynamics CRM.
+                        tracingService.Trace("FollowUpPlugin: Creating the task activity.");
+                        service.Create(followup);
+                    }
+
+                    catch (FaultException<OrganizationServiceFault> ex)
+                    {
+                        throw new InvalidPluginExecutionException(String.Format("An error occurred in FollowUpPlugin. {0}", ex.Message));
+                    }
+
+                    catch (Exception ex)
+                    {
+                        tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                        throw;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ ValidateAccountName.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPlugin
+{
+    public class ValidateAccountName : IPlugin
+    {
+        //Invalid names from unsecure configuration
+        private List<string> invalidNames = new List<string>();
+
+        // Constructor to capture the unsecure configuration
+        public ValidateAccountName(string unsecure)
+        {
+            // Parse the configuration data and set invalidNames
+            if (!string.IsNullOrWhiteSpace(unsecure))
+                unsecure.Split(',').ToList().ForEach(s =>
+                {
+                    invalidNames.Add(s.Trim());
+                });
+        }
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+
+                // Obtain the execution context from the service provider.  
+                IPluginExecutionContext context = (IPluginExecutionContext)
+                    serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                // Verify all the requirements for the step registration
+                if (context.InputParameters.Contains("Target") && //Is a message with Target
+                    context.InputParameters["Target"] is Entity && //Target is an entity
+                    ((Entity)context.InputParameters["Target"]).LogicalName.Equals("account") && //Target is an account
+                    ((Entity)context.InputParameters["Target"])["name"] != null && //account name is passed
+                    context.MessageName.Equals("Update") && //Message is Update
+                    context.PreEntityImages["a"] != null && //PreEntityImage with alias 'a' included with step
+                    context.PreEntityImages["a"]["name"] != null) //account name included with PreEntityImage with step
+                {
+                    // Obtain the target entity from the input parameters.  
+                    var entity = (Entity)context.InputParameters["Target"];
+                    var newAccountName = (string)entity["name"];
+                    var oldAccountName = (string)context.PreEntityImages["a"]["name"];
+
+                    if (invalidNames.Count > 0)
+                    {
+                        tracingService.Trace("ValidateAccountName: Testing for {0} invalid names:", invalidNames.Count);
+
+                        if (invalidNames.Contains(newAccountName.ToLower().Trim()))
+                        {
+                            tracingService.Trace("ValidateAccountName: new name '{0}' found in invalid names.", newAccountName);
+
+                            // Test whether the old name contained the new name
+                            if (!oldAccountName.ToLower().Contains(newAccountName.ToLower().Trim()))
+                            {
+                                tracingService.Trace("ValidateAccountName: new name '{0}' not found in '{1}'.", newAccountName, oldAccountName);
+
+                                string message = string.Format("You can't change the name of this account from '{0}' to '{1}'.", oldAccountName, newAccountName);
+
+                                throw new InvalidPluginExecutionException(message);
+                            }
+
+                            tracingService.Trace("ValidateAccountName: new name '{0}' found in old name '{1}'.", newAccountName, oldAccountName);
+                        }
+
+                        tracingService.Trace("ValidateAccountName: new name '{0}' not found in invalidNames.", newAccountName);
+                    }
+                    else
+                    {
+                        tracingService.Trace("ValidateAccountName: No invalid names passed in configuration.");
+                    }
+                }
+                else
+                {
+                    tracingService.Trace("ValidateAccountName: The step for this plug-in is not configured correctly.");
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace("BasicPlugin: {0}", ex.ToString());
+                throw;
+            }
+        }
+    }
+}
